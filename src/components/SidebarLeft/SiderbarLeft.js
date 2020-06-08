e587673923639334d517206617b0a50a46deee55
Maximiliano <maxicopp@gmail.com>
2020-06-08 16:33:25 -0300

    Rediseño barra lateral

--- src/components/SidebarLeft/SiderbarLeft.js
+++ src/components/SidebarLeft/SiderbarLeft.js
@@ -1,226 +1,103 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './styles.css'
-import { NavLink } from 'react-router-dom';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import Logout from '../Logout/Logout'
-import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+
+// Íconos de Material UI
 import HomeIcon from '@material-ui/icons/Home';
 import PersonIcon from '@material-ui/icons/Person';
+import PeopleIcon from '@material-ui/icons/People';
 import SecurityIcon from '@material-ui/icons/Security';
 import SettingsIcon from '@material-ui/icons/Settings';
-import { HELPER_FUNCTIONS } from '../../helpers/Helpers'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
-const drawerWidth = 240;
+export default class SiderbarLeft extends Component {
+    render() {
+        return (
+            <div className="side__bar">
+                <div className="side__bar_container side__bar_inverse">
+                    <ul className="parent__menu_holder side__bar_inverse">
+                        <li><HomeIcon /></li>
+                        <li><PersonIcon /></li>
+                        <li><PeopleIcon /></li>
+                        <li><SecurityIcon /></li>
+                        <li><SettingsIcon />
+                            <ul className="child__menu">
+                                <li>My New Design</li>
+                                <ul className="child__list">
+                                    <li><a href="#">Design One</a></li>
+                                    <li><a href="#">Design Two</a></li>
+                                    <li><a href="#">Design Three</a></li>
+                                    <li><a href="#">One Of My Best Design</a></li>
+                                    <li><a href="#">Check Out My Other Design www.niweshshrestha.com.np</a></li>
+                                    <li><a href="#">Design Four</a></li>
+                                    <li><a href="#">Design Three</a></li>
+                                    <li><a href="#">One Of My Best Design</a></li>
+                                    <li><a href="#">Check Out My Other Design www.niweshshrestha.com.np</a></li>
+                                    <li><a href="#">Design Four</a></li>
+                                </ul>
+                            </ul>
+                        </li>
+                        <li><ExitToAppIcon />
+                            <ul className="child__menu">
+                                <li>My New Design</li>
+                            </ul>
+                        </li>
+                        {/* <li><i className="fa fa-dropbox"></i>
+                            <ul className="child__menu">
+                                <li>My New Design</li>
+                            </ul>
+                        </li>
+                        <li><i className="fa fa-git"></i>
+                            <ul className="child__menu">
+                                <li>My New Design</li>
+                            </ul>
+                        </li>
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-    },
-    appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-        justifyContent: 'flex-end',
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: -drawerWidth,
-    },
-    contentShift: {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-    },
-}));
-
-export default function PersistentDrawerLeft() {
-    const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
-
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
-
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar
-                position="fixed"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}
-                style={{ background: '#2E3B55' }}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        edge="start"
-                        className={clsx(classes.menuButton, open && classes.hide)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" noWrap>
-                        Bienvenido usuario
-          </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                className={classes.drawer}
-                variant="persistent"
-                anchor="left"
-                open={open}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-            >
-                <div className={classes.drawerHeader}>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                    </IconButton>
+                        <li><i className="fa fa-amazon"></i>
+                            <ul className="child__menu">
+                                <li>My New Design</li>
+                            </ul>
+                        </li>
+                        <li><i className="fa fa-android"></i>
+                            <ul className="child__menu">
+                                <li>My New Design</li>
+                            </ul>
+                        </li>
+                        <li><i className="fa fa-apple"></i>
+                            <ul className="child__menu">
+                                <li>My New Design</li>
+                            </ul>
+                        </li>
+                        <li><i className="fa fa-behance"></i>
+                            <ul className="child__menu">
+                                <li>My New Design</li>
+                                <ul className="child__list">
+                                    <li><a href="#">Design One</a></li>
+                                    <li><a href="#">Design Two</a></li>
+                                    <li><a href="#">Design Three</a></li>
+                                    <li><a href="#">One Of My Best Design</a></li>
+                                    <li><a href="#">Check Out My Other Design www.niweshshrestha.com.np</a></li>
+                                    <li><a href="#">Design Four</a></li>
+                                </ul>
+                            </ul>
+                        </li>
+                        <li><i className="fa fa-css3"></i>
+                            <ul className="child__menu">
+                                <li>My New Design</li>
+                            </ul>
+                        </li>
+                        <li><i className="fa fa-dropbox"></i>
+                            <ul className="child__menu">
+                                <li>My New Design</li>
+                            </ul>
+                        </li>
+                        <li><i className="fa fa-git"></i>
+                            <ul className="child__menu">
+                                <li>My New Design</li>
+                            </ul>
+                        </li> */}
+                    </ul>
                 </div>
-                <Divider />
-                <List>
-                    <NavLink to="/home" activeClassName="active" style={{ textDecoration: 'none' }}>
-                        <ListItem button key={"Home"}>
-                            <ListItemIcon><HomeIcon /></ListItemIcon>
-                            <ListItemText primary={"home"} />
-                        </ListItem>
-                    </NavLink>
-                    {HELPER_FUNCTIONS.checkPermissionGroup("users") &&
-
-                        <NavLink to="/users" activeClassName="active" style={{ textDecoration: 'none' }}>
-                            <ListItem button key={"Usuarios"}>
-                                <ListItemIcon><PersonIcon /></ListItemIcon>
-                                <ListItemText primary={"usuarios"} />
-                            </ListItem>
-                        </NavLink>
-                    }
-
-                    {HELPER_FUNCTIONS.checkPermissionGroup("groups") &&
-                        <NavLink to="/groups" activeClassName="active" style={{ textDecoration: 'none' }}>
-                            <ListItem button key={"Grupos"}>
-                                <ListItemIcon><PeopleAltIcon /></ListItemIcon>
-                                <ListItemText primary={"grupos"} />
-                            </ListItem>
-                        </NavLink>
-                    }
-
-                    {HELPER_FUNCTIONS.checkPermissionGroup("roles") &&
-                        <NavLink to="/roles" activeClassName="active" style={{ textDecoration: 'none' }}>
-                            <ListItem button key={"Roles"}>
-                                <ListItemIcon><SecurityIcon /></ListItemIcon>
-                                <ListItemText primary={"roles"} />
-                            </ListItem>
-                        </NavLink>
-                    }
-
-                    {HELPER_FUNCTIONS.checkPermissionGroup("") &&
-
-                        <NavLink to="/backoffice" activeClassName="active" style={{ textDecoration: 'none' }}>
-                            <ListItem button key={"Backoffice"}>
-                                <ListItemIcon><SettingsIcon /></ListItemIcon>
-                                <ListItemText primary={"backoffice"} />
-                            </ListItem>
-                        </NavLink>
-                    }
-                </List>
-                <Divider />
-                <List>
-                    <ListItem button key={"otro"}>
-                        <ListItemIcon><ExitToAppIcon /></ListItemIcon>
-                        <Logout />
-                    </ListItem>
-                </List>
-            </Drawer>
-            <main
-                className={clsx(classes.content, {
-                    [classes.contentShift]: open,
-                })}
-            >
-                <div className={classes.drawerHeader} />
-                {/* <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-        </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography> */}
-            </main>
-        </div>
-    );
+            </div>
+        )
+    }
 }

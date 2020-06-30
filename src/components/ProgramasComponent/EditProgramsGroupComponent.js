3808d5d792783e6c0e766eab968cb6dd25b20c0d
Maximiliano Cóppola <maxicopp@gmail.com>
2020-06-30 16:20:58 -0300

    Avanzamos con programas

--- src/components/ProgramasComponent/EditProgramsGroupComponent.js
+++ src/components/ProgramasComponent/EditProgramsGroupComponent.js
@@ -5,8 +5,18 @@ import Global from '../../Global'
 import { HELPER_FUNCTIONS } from '../../helpers/Helpers'
 import axios from 'axios'
 import swal from 'sweetalert'
+import Searched from './Searched'
 
 export default class EditProgramsGroupComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ok: false,
+            modeloUsuario: null,
+            searchString: ''
+        }
+    }
+
     addUser = (event) => {
         event.preventDefault()
         let token = JSON.parse(sessionStorage.getItem('token'))
@@ -42,6 +52,14 @@ export default class EditProgramsGroupComponent extends Component {
 
     }
 
+    buscarUsuarios = (e) => {
+        e.preventDefault()
+        let searched = this.input.value
+        this.setState({
+            searchString: searched
+        })
+    }
+
     componentDidMount() {
         const { id, name, description } = this.props.location.state
         console.log("El id buscado: ", id)
@@ -49,27 +67,61 @@ export default class EditProgramsGroupComponent extends Component {
         const token = tokenUser
         const bearer = `Bearer ${token}`
         axios.get(Global.getAllProgramsGroups + '/' + id, { headers: { Authorization: bearer } }).then(response => {
-            console.log("cesar: ", response.data.Data[0].assignedUsers)
+            let arrayUsuarios = []
+            response.data.Data[0].assignedUsers.map(user => {
+                let modeloUsuario = {
+                    id: user.id,
+                    name: user.name,
+                    lastName: user.lastName
+                }
+
+                arrayUsuarios.push(modeloUsuario)
+            })
+
+            this.setState({ ok: true, modeloUsuario: arrayUsuarios });
+
+            console.log("Modelo: ", this.state.modeloUsuario)
             sessionStorage.setItem('token', JSON.stringify(response.data.loggedUser.token))
         })
     }
 
     render() {
         const { name, description } = this.props.location.state
-
-        console.log(name)
-        console.log(description)
+        if (this.state.modeloUsuario) {
+            console.log("En el render: ", this.state.modeloUsuario)
+        }
         return (
             <div>
                 <SidebarLeft />
                 <div>
+                    <form onSubmit={this.buscarUsuarios} className="inputsEditUser addUserPadding">
+                        <div>Usuarios asignados</div>
+                        {this.state.modeloUsuario &&
+                            this.state.modeloUsuario.map((user, id) => {
+                                return (
+                                    <div key={id}>
+                                        <div>ID: {user.id}</div>
+                                        <div>Nombre {user.name} {user.lastName}</div>
+                                        <hr />
+                                    </div>
+
+                                )
+                            })
+                        }
+                        <span className="Label">Agregar más usuarios</span>
+                        <input className="form-control" type="text" placeholder="" ref={(c) => this.input = c} />
+                        <button className="btn btn-block btn-info ripple-effect confirmar" type="submit" name="Submit" alt="sign in">Buscar</button>
+                    </form>
+                    {this.state.searchString !== '' &&
+                        <Searched searchString={this.state.searchString} />
+                    }
                     <form onSubmit={this.addUser} className="inputsEditUser addUserPadding">
                         <span className="Label">Nombre</span>
                         <input className="form-control" type="text" placeholder="" ref={(c) => this.name = c} defaultValue={name ? name : ''} />
                         <span className="Label">Descripción</span>
                         <input className="form-control" type="text" placeholder="" ref={(c) => this.description = c} defaultValue={description ? description : ''} />
-                        <span className="Label">Usuarios asignados</span>
-                        <SelectGroup getValue={(c) => this.usersAssign = c} />
+                        {/* <span className="Label">Usuarios asignados</span>
+                        <SelectGroup getValue={(c) => this.usersAssign = c} /> */}
                         <button className="btn btn-block btn-info ripple-effect confirmar" type="submit" name="Submit" alt="sign in">Crear Grupo de Programas</button>
                     </form>
                 </div>
---
+++ src/components/ProgramasComponent/Searched.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import Global from '../../Global'
+import {HELPER_FUNCTIONS} from '../../helpers/Helpers'
+import swal from 'sweetalert'
+
+export default class Searched extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            result: null
+        }
+    }
+
+    componentDidMount() {
+        const tokenUser = JSON.parse(sessionStorage.getItem("token"))
+        const token = tokenUser
+        const bearer = `Bearer ${token}`
+        axios.get(Global.getUsers, { headers: { Authorization: bearer } }).then(response => {
+            // console.log("usuarios: ", response.data.Data)
+            const usuarios = response.data.Data;
+            const resultado = usuarios.filter(usuario => {
+                return usuario.id === this.props.searchString || usuario.name === this.props.searchString.toLowerCase() || usuario.lastName === this.props.searchString.toLowerCase()
+            })
+            console.log(resultado)
+            this.setState({
+                result: resultado
+            })
+            sessionStorage.setItem("token", JSON.stringify(response.data.loggedUser.token));
+        })
+            .catch((e) => {
+                console.log(e)
+                // Si hay algún error en el request lo deslogueamos
+                this.setState({
+                    error: true,
+                    redirect: true
+                })
+                if (!e.response.data.Success && e.response.data.HttpCodeResponse === 401) {
+                    HELPER_FUNCTIONS.logout()
+                } else {
+                    sessionStorage.setItem('token', JSON.stringify(e.response.data.loggedUser.token))
+                    swal("Error!", "Hubo un problema", "error");
+                }
+                console.log("Error: ", e)
+            });
+    }
+    render() { 
+        return (
+            <div>
+                {this.state.result &&
+                    this.state.result.map((usuario, key) => {
+                        return (
+                            <div key={key} style={{textAlignVertical: "center",textAlign: "center",}}>
+                                <div>
+                                    {usuario.id} - {usuario.name} {usuario.lastName}
+                                </div>
+                                <div>Agregar</div>
+                                <hr />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}

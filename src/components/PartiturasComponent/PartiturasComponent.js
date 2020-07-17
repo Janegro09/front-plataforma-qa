d21a427c3a24f66c343a77689b1baae3f853fc12
Maximiliano Cóppola <maxicopp@gmail.com>
2020-07-17 15:30:03 -0300

    Componente de partituras

--- src/components/PartiturasComponent/PartiturasComponent.js
+++ src/components/PartiturasComponent/PartiturasComponent.js
@@ -1,13 +1,64 @@
 import React, { Component } from 'react'
-import SidebarLeft from '../SidebarLeft/SiderbarLeft'
+import SiderbarLeft from '../SidebarLeft/SiderbarLeft'
+import UserAdminHeader from '../Users/userAdminHeader/userAdminHeader'
 
 export default class PartiturasComponent extends Component {
     render() {
         return (
             <div>
-                <SidebarLeft />
+                <div className="header">
+                    {/* BOTON DE SALIDA */}
+                    {/* BARRA LATERAL IZQUIERDA */}
+                    <SiderbarLeft />
+                    <UserAdminHeader />
+                </div>
                 <div>
-                    Partituras works!!!
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Estado de partitura</th>
+                                <th>Perfilamientos</th>
+                                <th>Fechas</th>
+                                <th>Usuarios</th>
+                                <th>Instancias</th>
+                                <th>Ver</th>
+                                <th>Eliminar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>January</td>
+                                <td>$100</td>
+                                <td>January</td>
+                                <td>$100</td>
+                                <td>January</td>
+                                <td>$100</td>
+                                <td>ver</td>
+                                <td>eliminar</td>
+                            </tr>
+                            <tr>
+                                <td>January</td>
+                                <td>$100</td>
+                                <td>January</td>
+                                <td>$100</td>
+                                <td>January</td>
+                                <td>$100</td>
+                                <td>ver</td>
+                                <td>eliminar</td>
+                            </tr>
+                            <tr>
+                                <td>January</td>
+                                <td>$100</td>
+                                <td>January</td>
+                                <td>$100</td>
+                                <td>January</td>
+                                <td>$100</td>
+                                <td>ver</td>
+                                <td>eliminar</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         )

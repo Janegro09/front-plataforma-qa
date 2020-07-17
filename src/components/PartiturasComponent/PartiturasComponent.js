82c2ebfe2ff309b5cb98d5c11cd2cd5eff8ebbb1
Maximiliano Cóppola <maxicopp@gmail.com>
2020-07-17 19:02:00 -0300

    Get partituras

--- src/components/PartiturasComponent/PartiturasComponent.js
+++ src/components/PartiturasComponent/PartiturasComponent.js
@@ -16,8 +16,41 @@ export default class PartiturasComponent extends Component {
         }
     }
 
-    verPartitura = () => {
-        console.log("Ver partitura");
+    verPartitura = (id) => {
+        // Hacer rekest
+        this.setState({
+            loading: true
+        })
+
+        const tokenUser = JSON.parse(sessionStorage.getItem("token"))
+        const token = tokenUser
+        const bearer = `Bearer ${token}`
+        axios.get(Global.getAllPartitures + '/' + id, { headers: { Authorization: bearer } }).then(response => {
+            sessionStorage.setItem("token", JSON.stringify(response.data.loggedUser.token));
+            console.log("La response: ", response.data.Data)
+            // this.setState({
+            //     allPartitures: response.data.Data,
+            //     loading: false
+            // })
+
+        })
+            .catch((e) => {
+                // Si hay algún error en el request lo deslogueamos
+                if (!e.response.data.Success && e.response.data.HttpCodeResponse === 401) {
+                    HELPER_FUNCTIONS.logout()
+                } else {
+                    sessionStorage.setItem('token', JSON.stringify(e.response.data.loggedUser.token))
+                    this.setState({
+                        loading: false
+                    })
+                    swal("Error!", "Hubo un problema", "error");
+                }
+                console.log("Error: ", e)
+            });
+    }
+
+    crearPartitura = () => {
+        console.log("Crear partitura");
     }
 
     eliminarPartitura = () => {
@@ -59,7 +92,6 @@ export default class PartiturasComponent extends Component {
 
     render() {
         let { allPartitures, loading } = this.state;
-
         return (
             <div>
                 {loading &&
@@ -71,37 +103,45 @@ export default class PartiturasComponent extends Component {
                     <SiderbarLeft />
                     <UserAdminHeader />
                 </div>
+
+                <button
+                    onClick={
+                        (e) => {
+                            e.preventDefault();
+                            this.crearPartitura();
+                        }
+                    }
+                >
+                    Crear partitura
+                </button>
                 <div className="section-content">
                     {allPartitures !== null &&
                         <table>
                             <thead>
                                 <tr>
+                                    <th>Fechas</th>
                                     <th>Nombre</th>
                                     <th>Estado de partitura</th>
-                                    <th>Perfilamientos</th>
-                                    <th>Fechas</th>
-                                    <th>Usuarios</th>
-                                    <th>Instancias</th>
+                                    <th>Archivos incluídos</th>
                                     <th>Ver</th>
                                     <th>Eliminar</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {allPartitures.map((partiture, key) => {
+                                    console.log(partiture)
                                     return (
                                         <tr key={key}>
+                                            <td>{moment(partiture.dates.createdAt).format("DD/MM/YYYY HH:mm")}</td>
                                             <td>{partiture.name}</td>
                                             <td>{partiture.partitureStatus}</td>
-                                            <td>{partiture.perfilamientos}</td>
-                                            <td>{moment(partiture.dates.createdAt).format("DD/MM/YYYY")}</td>
-                                            <td>{partiture.users}</td>
-                                            <td>{partiture.instances}</td>
+                                            <td>{partiture.fileId.length.toString()}</td>
                                             <td>
                                                 <button
                                                     onClick={
                                                         (e) => {
                                                             e.preventDefault();
-                                                            this.verPartitura();
+                                                            this.verPartitura(partiture.id);
                                                         }
                                                     }
                                                 >

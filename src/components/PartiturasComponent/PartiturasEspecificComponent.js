915aa424677eedc188dbd5cfd11c1c97d1d5b0ee
Maximiliano Cóppola <maxicopp@gmail.com>
2020-07-20 19:55:20 -0300

    Avanzamos

--- src/components/PartiturasComponent/PartiturasEspecificComponent.js
+++ src/components/PartiturasComponent/PartiturasEspecificComponent.js
@@ -5,13 +5,14 @@ import { HELPER_FUNCTIONS } from '../../helpers/Helpers';
 import swal from 'sweetalert';
 import axios from 'axios';
 import Global from '../../Global';
+import moment from 'moment';
 
 export default class PartiturasEspecificComponent extends Component {
 
     state = {
         id: null,
         loading: false,
-        users: null
+        data: null
     }
 
     componentDidMount() {
@@ -28,9 +29,8 @@ export default class PartiturasEspecificComponent extends Component {
             sessionStorage.setItem("token", JSON.stringify(response.data.loggedUser.token));
             this.setState({
                 loading: false,
-                users: response.data.Data[0].users
+                data: response.data.Data
             })
-            console.log("La response: ", response.data.Data)
 
         })
             .catch((e) => {
@@ -47,8 +47,11 @@ export default class PartiturasEspecificComponent extends Component {
                 console.log("Error: ", e)
             });
     }
+
     render() {
-        let { loading, users } = this.state;
+        let { loading, data } = this.state;
+        data = data ? data[0] : null;
+
         return (
             <>
                 {loading &&
@@ -61,47 +64,64 @@ export default class PartiturasEspecificComponent extends Component {
                     <UserAdminHeader />
                 </div>
 
-                <div className="section-content">
-                    <h1>Archivo actual</h1>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Fechas</th>
-                                <th>Nombre</th>
-                                <th>Estado de partitura</th>
-                                <th>Archivos incluídos</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>a</td>
-                                <td>a</td>
-                                <td>a</td>
-                                <td>a</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                {data &&
+                    <div className="section-content">
+                        <h1>Archivo actual</h1>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Fechas</th>
+                                    <th>Nombre</th>
+                                    <th>Estado de partitura</th>
+                                    <th>Archivos incluídos</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{moment(data.dates.createdAt).format("DD/MM/YYYY HH:mm")}</td>
+                                    <td>{data.name}</td>
+                                    <td>{data.partitureStatus}</td>
+                                    <td>{data.fileId.length}</td>
+                                </tr>
+                            </tbody>
+                        </table>
 
-                    <h2>Usuarios</h2>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Fechas</th>
-                                <th>Nombre</th>
-                                <th>Estado de partitura</th>
-                                <th>Archivos incluídos</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>a</td>
-                                <td>a</td>
-                                <td>a</td>
-                                <td>a</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                        <h2>Usuarios</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>DNI</th>
+                                    <th>Improvment</th>
+                                    <th>Estado</th>
+                                    <th>Nombre</th>
+                                    <th>Canal</th>
+                                    <th>Última actualización</th>
+                                    <th>Cluster</th>
+                                    <th>Responsable</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data.users &&
+                                    data.users.map(user => {
+                                        return (
+                                            <tr key={user.idDB}>
+                                                <td><button>Ver</button></td>
+                                                <td>{user.dni}</td>
+                                                <td>{user.improvment}</td>
+                                                <td>{user.partitureStatus}</td>
+                                                <td>{user.name} {user.lastName}</td>
+                                                <td>{user.canal}</td>
+                                                <td>REVISAR ESTA CACA</td>
+                                                <td>{user.cluster}</td>
+                                                <td>{user.responsable}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                }
             </>
         )
     }

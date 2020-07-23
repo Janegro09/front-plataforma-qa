01bc08a87fa795408ebb9c0db94974862408e2a3
Maximiliano <maxicopp@gmail.com>
2020-07-23 16:10:17 -0300

    Fin StepName

--- src/components/PartiturasComponent/StepName.js
+++ src/components/PartiturasComponent/StepName.js
@@ -5,12 +5,86 @@ import Global from '../../Global';
 import axios from 'axios';
 import { HELPER_FUNCTIONS } from '../../helpers/Helpers';
 import swal from 'sweetalert';
+import moment from 'moment';
 
 export default class StepName extends Component {
 
     state = {
         loading: false,
         data: null
+    }
+
+    getUsersColumns() {
+        let { data, redirect } = this.state;
+
+        if (redirect) {
+            let { id, idUsuario } = this.props.match.params;
+            console.log("redirigir")
+        }
+
+        data = data ? data[0] : null;
+        let users = data ? data.users[0] : null
+
+        let ReturnData = {
+            headers: [],
+            actual: [],
+            comparativo: []
+        }
+
+        for (let th in users.rowFromPartiture) {
+            if (th === 'id') continue;
+            const value = users.rowFromPartiture[th]
+            ReturnData.headers.push(th)
+            ReturnData.actual.push(value);
+        }
+        /**
+         * Cuando se haga reporteria, va a venir una columna mas con la misma sintaxisss que la de arriba para comparar
+         */
+        // for (let th in users.rowFromPartiture) {
+        //     if(th === 'id') continue;
+        //     const value = users.rowFromPartiture[th]
+        //     ReturnData.comparativo.push(value);
+        // }
+
+        return (
+            <table className="longXTable">
+                <thead>
+                    {ReturnData.headers.length > 0 &&
+                        <tr>
+                            <th>Estado</th>
+                            <th>Improvment</th>
+                            {ReturnData.headers.map((value, key) => {
+                                return <th key={key}>{value}</th>
+                            })
+
+                            }
+                        </tr>
+                    }
+                </thead>
+                <tbody>
+                    {ReturnData.actual.length > 0 &&
+                        <tr>
+                            <td>{users.partitureStatus}</td>
+                            <td>{users.improvment}</td>
+                            {ReturnData.actual.map((value, key) => {
+                                return <th key={key}>{value}</th>
+                            })
+                            }
+                        </tr>
+                    }
+                    {ReturnData.comparativo.length > 0 &&
+                        <tr>
+                            <td>January</td>
+                            <td>$100</td>
+                            {ReturnData.comparativo.map((value, key) => {
+                                return <th key={key}>{value}</th>
+                            })
+                            }
+                        </tr>
+                    }
+                </tbody>
+            </table>
+        )
     }
 
     componentDidMount() {
@@ -49,8 +123,11 @@ export default class StepName extends Component {
 
     render() {
         let { data } = this.state;
-
+        data = data ? data[0] : null;
         console.log(data);
+
+        let date = new Date();
+        date = moment(date);
 
         return (
             <>
@@ -60,6 +137,68 @@ export default class StepName extends Component {
                     <SiderbarLeft />
                     <UserAdminHeader />
                 </div>
+
+                {data &&
+                    <div className="section-content">
+                        <h1>Archivo actual</h1>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Fechas</th>
+                                    <th>Nombre</th>
+                                    <th>Estado de partitura</th>
+                                    <th>Archivos incluídos</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{moment(data.dates.createdAt).format("DD/MM/YYYY HH:mm")}</td>
+                                    <td>{data.name}</td>
+                                    <td>{data.partitureStatus}</td>
+                                    <td>{data.fileId.length}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <h2>Usuarios Actuales</h2>
+                        {this.getUsersColumns()}
+
+                        <section>
+                            {data.instances &&
+                                data.instances.map(v =>
+                                    (
+                                        <article key={v.id}>
+                                            <h6>{v.name}</h6>
+                                            <p className={!moment(v.dates.expirationDate).isBefore(date) ? 'fecha' : 'fecha vencido'}>{moment(v.dates.expirationDate).format("DD/MM/YYYY")}</p>
+                                            <div className="steps">
+                                                {v.steps.length > 0 &&
+                                                    v.steps.map(s => (
+                                                        <span key={s.id}>
+                                                            <input
+                                                                type="checkbox"
+                                                                onChange={() => {
+                                                                    this.modificarEstado(s.id);
+                                                                }}
+                                                                defaultChecked={s.completed} />
+                                                            <p onClick={(e) => {
+                                                                e.preventDefault();
+                                                                this.goToStep(s.id);
+                                                            }}>{s.name}</p>
+                                                            <p>{s.requestedMonitorings}</p>
+                                                        </span>
+                                                    ))
+                                                }
+                                            </div>
+                                        </article>
+                                    )
+                                )
+
+                            }
+
+                        </section>
+                    </div>
+                }
+
             </>
         )
     }

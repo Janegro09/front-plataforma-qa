4029a1991b874faa807fb5d9871d1e0a6580e082
root <root@solucionesdigitalesteco.com.ar>
2020-07-29 16:09:47 -0300

    kok0

---
+++ build/precache-manifest.be7a5fbf14030e9b5301b70b309d95c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cddf25771890b1e0de40fb48b30e5c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65b3852fed3ae27d623d",
+    "url": "/static/css/main.76fde104.chunk.css"
+  },
+  {
+    "revision": "9ed80f972d1305948045",
+    "url": "/static/js/2.b8b9632c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b8b9632c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b3852fed3ae27d623d",
+    "url": "/static/js/main.9b87a5e4.chunk.js"
+  },
+  {
+    "revision": "56004f45e49cc7b47e16",
+    "url": "/static/js/runtime-main.6c882538.js"
+  },
+  {
+    "revision": "1046c23a552f3680632f2cb433f47a32",
+    "url": "/static/media/logo_background.1046c23a.png"
+  },
+  {
+    "revision": "61ea26c704e10c2f27ba196c552a37f4",
+    "url": "/static/media/qa_logos.61ea26c7.png"
+  }
+]);

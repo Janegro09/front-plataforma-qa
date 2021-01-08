fe8807ab988fbd4c177de1316e66aad05a29ddc1
Alfredo Gelberg <gelberg_3@hotmail.com>
2021-01-08 13:04:48 -0300

    se modifica coordionador On Site por On Site / Coordinador / Coordinador OC

--- src/components/PartiturasComponent/StepName.js
+++ src/components/PartiturasComponent/StepName.js
@@ -685,7 +685,49 @@ export default class StepName extends Component {
 
                                         {this.state.role !== false && this.state.role !== 'REPRESENTANTE' && this.state.role !== 'LIDER' && this.state.role !== 'RESPONSABLE' &&
                                             <article>
-                                                <h6 className="titulo-seccion">Coordinador On Site</h6>
+                                                <h6 className="titulo-seccion">Coordinador</h6>
+                                                <hr />
+                                                {customFields &&
+                                                    <CustomFields
+                                                        disabled={this.state.role !== 'GERENTE' && this.state.role !== 'ADMINISTRATOR'}
+                                                        fields={customFields}
+                                                        section='P'
+                                                        subsection='COR'
+                                                        values={step.coordinatorComments}
+                                                        data={(d) => {
+                                                            this.armarObjeto('coordinatorComments', d)
+                                                        }}
+                                                    />
+                                                }
+                                            </article>
+
+                                        }
+                                        
+
+                                        {this.state.role !== false && this.state.role !== 'REPRESENTANTE' && this.state.role !== 'LIDER' && this.state.role !== 'RESPONSABLE' &&
+                                            <article>
+                                                <h6 className="titulo-seccion">Coordinador OC</h6>
+                                                <hr />
+                                                {customFields &&
+                                                    <CustomFields
+                                                        disabled={this.state.role !== 'GERENTE' && this.state.role !== 'ADMINISTRATOR'}
+                                                        fields={customFields}
+                                                        section='P'
+                                                        subsection='COC'
+                                                        values={step.coordinatorOCComments}
+                                                        data={(d) => {
+                                                            this.armarObjeto('coordinatorOCComments', d)
+                                                        }}
+                                                    />
+                                                }
+                                            </article>
+
+                                        }
+
+
+                                        {this.state.role !== false && this.state.role !== 'REPRESENTANTE' && this.state.role !== 'LIDER' && this.state.role !== 'RESPONSABLE' &&
+                                            <article>
+                                                <h6 className="titulo-seccion">On Site</h6>
                                                 <hr />
                                                 {customFields &&
                                                     <CustomFields
